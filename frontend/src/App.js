cc00d0327213bec0e3ddf08d9f631b7773c8b200
mrgeekay727 <mrgeekay727@gmail.com>
2026-03-28 10:40:10 +0200

    Create App.js for manga generator interface

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Manga Generator Interface</h1>
+            <input type="text" placeholder="Enter manga title" />
+            <button>Generate</button>
+        </div>
+    );
+};
+
+export default App;
